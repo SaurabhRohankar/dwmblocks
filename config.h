2cb3f5ded608f81d2e35cac17fdd5d6c98da8a15
Your Name <you@example.com>
2024-09-15 22:25:17 +0530

    dwmblocks config

--- config.h
+++ config.h
@@ -2,10 +2,12 @@
 static const Block blocks[] = {
 
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"",  "nettraf",  1,  16}, 
+  {"", "memory", 10, 20},
+/*  {"",  "nettraf",  1,  16}, */
   {"",  "volume", 1,  10},
-  {"",  "clock",  60, 1},
   {"",  "internet", 5,  4},
+  {"",  "battery",  60,  1}, 
+  {"",  "clock",  60, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
